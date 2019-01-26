39d6f5dc4fe275a2f489a1c200f28f8b833e4e47
abby-shabi <abbyshabi@gmail.com>
2019-01-26 15:22:43 +0300

    initialized the setup method in the credentials test class

--- credential_test.py
+++ credential_test.py
@@ -1,2 +1,14 @@
 import unittest
 from credentials import Credentials
+
+class TestCredential(unittest.TestCase):
+    '''
+        The test credential class defines test cases for the credential class
+    '''
+
+    def setUp(self):
+        """
+        The setUp method will run before each test case
+        """
+        self.new_account = Credentials('Twitter','fog123')
+        
