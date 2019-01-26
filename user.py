ff1665202ab2ab19cd500c7cfc93dbdece1daee5
abby-shabi <abbyshabi@gmail.com>
2019-01-26 14:29:44 +0300

    created the save user method

--- user.py
+++ user.py
@@ -2,7 +2,7 @@ class User:
         '''
          the user class holds the user information and password
         '''
-        users=[]
+        user_list = []
 def _init_(self,first_name,user_name,password):
         '''__init__ methods helps us define properties for out objects
         '''
@@ -10,6 +10,13 @@ def _init_(self,first_name,user_name,password):
         self.first_name = first_name
         self.user_name = user_name
         self.password = password
+
+def save_user(self):
+        '''
+        save_user information
+        '''
+        User.user_list.append(self)
+
 
 
 
