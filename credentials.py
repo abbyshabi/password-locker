cb56647bf9b13f31d821c0170e83268cc2f254b6
abby-shabi <abbyshabi@gmail.com>
2019-01-26 15:13:55 +0300

    created the array for account_list

--- credentials.py
+++ credentials.py
@@ -1,4 +1,9 @@
 class Credentials:
     ''' The credentials class would hold all the information of the user
     '''
-   
+      account_list = []
+    def __init__(self,user_account,account_password):
+         self.user_account = user_account
+         self.account_password = account_password
+
+         
