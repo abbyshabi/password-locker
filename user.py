f87b16663be9b8f718c279c364891300a97a4968
abby-shabi <abbyshabi@gmail.com>
2019-01-26 14:25:57 +0300

    declared the properties of the user and assigned variables

--- user.py
+++ user.py
@@ -1,6 +1,15 @@
 class User:
+        '''
+         the user class holds the user information and password
+        '''
+        users=[]
+def _init_(self,first_name,user_name,password):
+        '''__init__ methods helps us define properties for out objects
+        '''
+      
+        self.first_name = first_name
+        self.user_name = user_name
+        self.password = password
 
-    users=[]
-    def _init_(self,first_name,username,password):
-    '''used the init method to define properties of my class user
-    '''
+
+
