fc871f11ee5e06ae0d53f0aad2cd347716ce6c8e
abby-shabi <abbyshabi@gmail.com>
2019-01-26 15:25:39 +0300

    adde the init test

--- credential_test.py
+++ credential_test.py
@@ -11,4 +11,13 @@ class TestCredential(unittest.TestCase):
         The setUp method will run before each test case
         """
         self.new_account = Credentials('Twitter','fog123')
-        
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_account.account_name,"Twitter")
+        self.assertEqual(self.new_account.password,"fog123")  
+      
+
